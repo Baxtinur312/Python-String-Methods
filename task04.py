b6512d74fac33755bbc1a072f82ab894fb634f0e
Baxtinur312 <baxtinururolov95@gmail.com>
2025-05-30 08:18:28 +0500

    Update task04.py

--- task04.py
+++ task04.py
@@ -1,4 +1,4 @@
 text1 = "python"
-print(text1.islower())  # True, because all characters are lowercase
+print(text1.islower())  
 text2 = "Python"
-print(text2.islower())  # False, because it contains an uppercase 'P'
+print(text2.islower()) 
